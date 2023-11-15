ecac083b0c153164e53bb678763b5fb5339e3d08
Lasse Vilpponen <lasse.vilpponen@gmail.com>
2023-11-15 18:04:27 +0200

    First code added

---
+++ src/index.py
@@ -0,0 +1,48 @@
+def formatInput(inputAuthor, inputTitle, inputPublisher, inputAddress, inputYear, inputKey):
+
+  formated = "@book{"+inputKey
+  formated += "\n  author    = \""+inputAuthor
+  formated += "\",\n  title     = \""+inputTitle
+  formated += "\",\n  publisher = \""+inputPublisher
+  formated += "\",\n  address   = \""+inputAddress
+  formated += "\",\n  year      = "+inputYear
+  formated += "\n}"
+
+  return formated
+
+
+def main():
+
+  book = """@book{CitekeyBook,
+    author    = "Leonard Susskind and George Hrabovsky",
+    title     = "Classical mechanics: the theoretical minimum",
+    publisher = "Penguin Random House",
+    address   = "New York, NY",
+    year      = 2014
+  }"""
+
+  
+
+  while True:
+    inputString = input('Do you want to add a reference (yes/ no)')
+    if inputString == "yes":
+      inputAuthor = input("Give the name of the author: ")
+      inputTitle = input("Give the book title: ")
+      inputPublisher = input("Give the name of the publisher: ")
+      inputAddress = input("Give the address of the publisher: ")
+      inputYear = input("Give the year of the publishing: ")
+      inputKey = input("Give key name to book in latex: ")
+
+      print(formatInput(inputAuthor, inputTitle, inputPublisher, inputAddress, inputYear, inputKey))
+
+    elif inputString == "no":
+      break
+    else: 
+      break  
+
+
+
+
+
+if __name__=='__main__':
+  main()
