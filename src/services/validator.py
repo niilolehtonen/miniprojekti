bab493869f997248fb744f3f493b3d96d9b91760
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-11-22 17:55:03 +0200

    fix validator

--- src/services/validator.py
+++ src/services/validator.py
@@ -8,46 +8,19 @@ class Validator:
     def __init__(self):
         pass
 
-    def validate_book(author,title,publisher,year,volume,series,address,edition,month,note):
-        if author != str or author == "":
-            raise UserInputError("Input not accepted on field 'Author'")
-        if title != str or title == "":
-            raise UserInputError("Input not accepted on field 'Title'")
-        if publisher != str or publisher == "":
-            raise UserInputError("Input not accepted on field 'Publisher'")
-        if year != int or year == "":
-            raise UserInputError("Input not accepted on field 'Year'")
-        if volume != int:
-            raise UserInputError("Input not accepted on field 'Volume'")
-        if series != str:
-            raise UserInputError("Input not accepted on field 'Series'")
-        if address != str:
-            raise UserInputError("Input not accepted on field 'Address'")
-        if edition != int:
-            raise UserInputError("Input not accepted on field 'Edition'")
-        if month != int:
-            raise UserInputError("Input not accepted on field 'Month'")
-        if note != str:
-            raise UserInputError("Input not accepted on field 'Note'")
-        
+    def validate_book(self,info):
+        if info["author"] == None:
+            raise UserInputError("Author field is empty")
+        if info["title"] == None:
+            raise UserInputError("Title field is empty")
+        if info["publisher"] == None:
+            raise UserInputError("Publisher field is empty")
+        if info["year"] == None:
+            raise UserInputError("Year field is empty")
         return None
     
-    def validate_manual(author,title,year,organization,address,edition,month,note):
-        if author != str or author == "":
-            raise UserInputError("Input not accepted on field 'Author'")
-        if title != str:
-            raise UserInputError("Input not accepted on field 'Title'")
-        if  organization != str:
-            raise UserInputError("Input not accepted on field 'Organization'")
-        if year != int:
-            raise UserInputError("Input not accepted on field 'Year'")
-        if address != str:
-            raise UserInputError("Input not accepted on field 'Address'")
-        if edition != int:
-            raise UserInputError("Input not accepted on field 'Edition'")
-        if month != int:
-            raise UserInputError("Input not accepted on field 'Month'")
-        if note != str:
-            raise UserInputError("Input not accepted on field 'Note'")
+    def validate_manual(self,info):
+        if info["title"] == None:
+            raise UserInputError("Title field is empty")
         
         return None
