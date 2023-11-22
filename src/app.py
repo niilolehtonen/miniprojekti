3b6062d209ab6c059424d151f0971e18121bba90
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2023-11-22 17:51:00 +0200

    Add infoDict

--- src/app.py
+++ src/app.py
@@ -19,6 +19,7 @@ def index():
 def addbook():
     return render_template("addbook.html")
 
+
 @app.route("/addmanual", methods=["GET"])
 def addmanual():
     return render_template("addmanual.html")
@@ -26,41 +27,50 @@ def addmanual():
 
 @app.route("/addbook", methods=["POST"])
 def addbook_submit():
-    author = request.form["author"]
-    title = request.form["title"]
-    publisher = request.form["publisher"]
-    year = request.form["year"]
-    volume = request.form["volume"]
-    series = request.form["series"]
-    address = request.form["address"]
-    edition = request.form["edition"]
-    month = request.form["month"]
-    note = request.form["note"]
+    information_dict = dict()
+
+    information_dict["author"] = request.form["author"] or None
+    information_dict["title"] = request.form["title"] or None
+    information_dict["publisher"] = request.form["publisher"] or None
+    information_dict["year"] = request.form["year"] or None
+    information_dict["volume"] = request.form["volume"] or None
+    information_dict["series"] = request.form["series"] or None
+    information_dict["address"] = request.form["address"] or None
+    information_dict["edition"] = request.form["edition"] or None
+    information_dict["month"] = request.form["month"] or None
+    information_dict["note"] = request.form["note"] or None
 
     validator = Validator()
-    validator.validate_book(author,title,publisher,year,volume,series,address,edition,month,note)
-    book = Book(author, title, publisher, int(year), int(volume), series, address, int(edition), int(month), note)
+    validator.validate_book(information_dict)
+    book = Book(information_dict["author"], information_dict["title"], information_dict["publisher"],
+                int(information_dict["year"]), int(information_dict["volume"]), information_dict["series"],
+                information_dict["address"], int(information_dict["edition"]), int(information_dict["month"]),
+                information_dict["note"])
     saver = ReferenceRepository("data.bib")
     saver.save_book(book)
     return redirect("/")
 
+
 @app.route("/addmanual", methods=["POST"])
 def addmanual_submit():
-    author = request.form["author"]
-    title = request.form["title"]
-    year = request.form["year"]
-    organization = request.form["organization"]
-    address = request.form["address"]
-    edition = request.form["edition"]
-    month = request.form["month"]
-    note = request.form["note"]
+    information_dict = dict()
+
+    information_dict["author"] = request.form["author"] or None
+    information_dict["title"] = request.form["title"] or None
+    information_dict["year"] = request.form["year"] or None
+    information_dict["organization"] = request.form["organization"] or None
+    information_dict["address"] = request.form["address"] or None
+    information_dict["edition"] = request.form["edition"] or None
+    information_dict["month"] = request.form["month"] or None
+    information_dict["note"] = request.form["note"] or None
 
     validator = Validator()
-    validator.validate_manual(author,title,year,organization,address,edition,month,note)
-    manual = Manual(title, int(year), author, organization, address, int(edition), int(month), note)
+    validator.validate_manual(information_dict)
+    manual = Manual(information_dict["title"], int(information_dict["year"]), information_dict["author"], information_dict["organization"], information_dict["address"], information_dict["edition"], information_dict["month"], information_dict["note"])
     saver = ReferenceRepository("data.bib")
     saver.save_manual(manual)
     return redirect("/")
+
 
 @app.route("/all_references")
 def all_references():
