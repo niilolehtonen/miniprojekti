b83a172c4217a406c2ee388a96bed5358c660898
Lasse Vilpponen <lasse.vilpponen@gmail.com>
2023-11-22 16:52:31 +0200

    add manual template

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from os import getenv
 
 from entities.book import Book
+from entities.manual import Manual
 from services.file_fetcher import FileFetcher
 from services.file_saver import FileSaver
 
@@ -17,6 +18,10 @@ def index():
 @app.route("/addbook", methods=["GET"])
 def addbook():
     return render_template("addbook.html")
+
+@app.route("/addmanual", methods=["GET"])
+def addmanual():
+    return render_template("addmanual.html")
 
 
 @app.route("/addbook", methods=["POST"])
@@ -36,6 +41,20 @@ def addbook_submit():
     saver.save(book)
     return redirect("/")
 
+@app.route("/addmanual", methods=["POST"])
+def addmanual_submit():
+    author = request.form["author"]
+    title = request.form["title"]
+    year = request.form["year"]
+    organization = request.form["organization"]
+    address = request.form["address"]
+    edition = request.form["edition"]
+    month = request.form["month"]
+    note = request.form["note"]
+    manual = Manual(title, int(year), author, organization, address, int(edition), int(month), note)
+    saver = FileSaver("data.bib")
+    saver.save(manual)
+    return redirect("/")
 
 @app.route("/all_references")
 def all_references():
