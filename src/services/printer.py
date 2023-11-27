622924059dfec056f44fbe0e099590e6c4b05d9c
Lasse Vilpponen <lasse.vilpponen@gmail.com>
2023-11-27 17:34:18 +0200

    Printer modified

--- src/services/printer.py
+++ src/services/printer.py
@@ -1,10 +1,16 @@
 class Printer:
 
-    def __init__(self):
-        pass
+    def __init__(self, reference_repository):
+        self.reference_repository = reference_repository
 
 
-    #def print_one()
+    #def print_one(self, entry_object)
 
-    #def list_references()
+        # for field in entry_object:    # Create schema here
+            # print(field)
 
+
+    #def list_references(self)          # Fetch all from repository
+
+        # for entry in self.reference_repository.fetch()
+            # print_one(entry)
