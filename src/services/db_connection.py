d861b2ccecdbee3d56d667a2b744f0bddda8ce4f
hakkajoe <joelhakkarainen95@gmail.com>
2023-12-01 17:11:55 +0200

    file save test

--- src/services/db_connection.py
+++ src/services/db_connection.py
@@ -2,5 +2,9 @@ import sqlite3
 
 connection = sqlite3.connect("database.db",check_same_thread=False)
 connection.row_factory = sqlite3.Row
-def get_database_connection():
-    return connection
+def get_database_connection(testing=False):
+        if testing:
+            testconnection = sqlite3.connect("testdatabase.db", check_same_thread=False)
+            testconnection.row_factory = sqlite3.Row
+            return testconnection
+        return connection
--- src/tests/file_fetcher_test.py
+++ src/tests/file_fetcher_test.py
@@ -1,9 +1,10 @@
 import unittest
-
 from entities.entry import Entry
 from repositories.reference_repository import ReferenceRepository
 from entities.book import Book
 from entities.manual import Manual
+from services.db_connection import get_database_connection
+from initialize_database import create_tables, drop_tables
 
 import os
 
@@ -12,9 +13,14 @@ class MockKeygen:
     def generate_key(self, title):
         return f"{title.split(' ')[0]}{999}"
 
-
 class TestBook(unittest.TestCase):
     def setUp(self):
+
+        connection = get_database_connection(testing=True)
+        self.repo = ReferenceRepository(connection)
+        drop_tables(connection)
+        create_tables(connection)
+
         book_data = {"author": "a",
                      "title": "b",
                      "publisher": "c",
@@ -40,7 +46,6 @@ class TestBook(unittest.TestCase):
 
         self.book = Entry(book_data, Book(), MockKeygen())
         self.manual = Entry(manual_data, Manual(), MockKeygen())
-        self.repo = ReferenceRepository("testdata.bib")
 
         self.repo.save_entry(self.book)
         self.repo.save_entry(self.manual)
