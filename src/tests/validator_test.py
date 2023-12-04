587fe24d20d4c6d0f17d9e7545f9bf748b65e64e
Soki <soki.fin02@gmail.com>
2023-12-04 16:55:47 +0200

    validator_test updated

--- src/tests/validator_test.py
+++ src/tests/validator_test.py
@@ -25,6 +25,71 @@ class ValidatorTest(unittest.TestCase):
         with self.assertRaises(UserInputError):
             self.validator.validate_book(self.info)
 
+    def test_validate_book_title_not_given(self):
+        self.info["author"] = "Timo"
+        self.info["title"] = None
+        self.info["publisher"] = "MIT"
+        self.info["year"] = "2018"
+
+        with self.assertRaises(UserInputError):
+            self.validator.validate_book(self.info)    
+
+    def test_validate_book_publisher_not_given(self):
+        self.info["author"] = "Timo"
+        self.info["title"] = "Algorimit"
+        self.info["publisher"] = None
+        self.info["year"] = "2018"
+
+        with self.assertRaises(UserInputError):
+            self.validator.validate_book(self.info)
+
+    def test_validate_book_year_not_given(self):
+        self.info["author"] = "Timo"
+        self.info["title"] = "Algorimit"
+        self.info["publisher"] = "MIT"
+        self.info["year"] = None
+
+        with self.assertRaises(UserInputError):
+            self.validator.validate_book(self.info)
+
+    def test_validate_book_invalid_author(self):
+        self.info["author"] = "34"
+        self.info["title"] = "Algorimit"
+        self.info["publisher"] = "MIT"
+        self.info["year"] = "2018"
+
+        errors = self.validator.validate_book(self.info)
+        self.assertIn("Author should contain only letters", errors)
+
+    def test_validate_book_invalid_year(self):
+        self.info["author"] = "Timo"
+        self.info["title"] = "Algorimit"
+        self.info["publisher"] = "MIT"
+        self.info["year"] = "year"
+
+        errors = self.validator.validate_book(self.info)
+        self.assertIn("Year should contain only numbers", errors)
+
+    def test_validate_book_invalid_year(self):
+        self.info["author"] = "Timo"
+        self.info["title"] = "Algorimit"
+        self.info["publisher"] = "MIT"
+        self.info["year"] = "2018"
+        self.info["volume"] = "first"
+
+        errors = self.validator.validate_book(self.info)
+        self.assertIn("Volume should contain only numbers", errors)
+
+    def test_validate_book_invalid_month(self):
+        self.info["author"] = "Timo"
+        self.info["title"] = "Algorimit"
+        self.info["publisher"] = "MIT"
+        self.info["year"] = "year"
+        self.info["month"] = "20"
+
+        errors = self.validator.validate_book(self.info)
+        self.assertIn("Month should contain only numbers 1 to 12", errors)
+
     def test_validate_manual_correct(self):
         self.info["title"] = "Ohjeet"
 
@@ -35,3 +100,24 @@ class ValidatorTest(unittest.TestCase):
 
         with self.assertRaises(UserInputError):
             self.validator.validate_manual(self.info)
+
+    def test_validate_manual_invalid_author(self):
+        self.info["title"] = "Ohjeet"
+        self.info["author"] = "23"
+
+        errors = self.validator.validate_manual(self.info)
+        self.assertIn("Author should contain only letters", errors)
+
+    def test_validate_manual_invalid_year(self):
+        self.info["title"] = "Ohjeet"
+        self.info["year"] = "vuosi"
+
+        errors = self.validator.validate_manual(self.info)
+        self.assertIn("Year should contain only numbers", errors)
+
+    def test_validate_manual_invalid_month(self):
+        self.info["title"] = "Ohjeet"
+        self.info["month"] = "23"
+
+        errors = self.validator.validate_manual(self.info)
+        self.assertIn("Month should contain only numbers 1 to 12", errors)
