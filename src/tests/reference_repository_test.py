2452d9b3544e659c3e7632d9c316a05dce82f858
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-12-04 16:48:41 +0200

    Tests for reference repository

---
+++ src/tests/reference_repository_test.py
@@ -0,0 +1,103 @@
+import os
+import unittest
+
+from entities.book import Book
+from entities.manual import Manual
+from entities.entry import Entry
+from repositories.reference_repository import ReferenceRepository
+from services.db_connection import get_database_connection, remove_test_database
+from initialize_database import create_tables, drop_tables
+
+
+class MockKeygen:
+    def generate_key(self, title):
+        return f"{title.split(' ')[0]}{999}"
+
+
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        self.testconnection = get_database_connection(testing=True)
+        self.repo = ReferenceRepository(self.testconnection, MockKeygen())
+        drop_tables(self.testconnection)
+        create_tables(self.testconnection)
+
+        book_data = {"author": "a",
+                "title": "b",
+                "publisher": "c",
+                "year": 2014,
+                "volume": 1,
+                "series": "d",
+                "address": "e",
+                "edition": 2,
+                "month": 3,
+                "note": "f"}
+        
+        manual_data = {"title": "a",
+                       "author": "c",
+                       "year": 2000,
+                       "month": "g",
+                       "address": "e",
+                       "note": "h",
+                       "organization": "d",
+                       "edition": "f"
+
+                       }
+
+        self.book = Entry(book_data, Book(), MockKeygen())
+        self.manual = Entry(manual_data, Manual(), MockKeygen())
+
+    def test_save(self):
+        correct_answer = [
+            'book entry: ',
+            'author: a',
+            'title: b',
+            'publisher: c',
+            'year: 2014',
+            'volume: 1',
+            'series: d',
+            'address: e',
+            'edition: 2',
+            'month: 3',
+            'note: f',
+            '',
+            '',
+            '',
+        ]
+
+        self.repo.save_entry(self.book)
+        f = self.repo.as_human_readable()
+        f = f.split("\n")
+        self.assertEqual(f, correct_answer)
+    
+    def test_fetch(self):
+        self.repo.save_entry(self.book)
+        self.repo.save_entry(self.manual)
+        correct_answer = """@book{b999,
+  author = "a",
+  title = "b",
+  publisher = "c",
+  year = 2014,
+  volume = 1,
+  series = "d",
+  address = "e",
+  edition = 2,
+  month = 3,
+  note = "f"
+}@manual{a999,
+  title = "a",
+  year = 2000,
+  month = "g",
+  address = "e",
+  note = "h",
+  organization = "d",
+  edition = "f",
+  author = "c"
+}"""
+        f = self.repo.fetch()
+        print(f)
+        self.assertEqual(f, correct_answer)
+
+
+
+    def tearDown(self):
+        remove_test_database(self.testconnection)
