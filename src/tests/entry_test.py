d599ffd64864f23f1cca69fcd005e6a83108e366
Vilpponen Lasse M <lassevil@cubbli22-gold.local>
2023-12-09 00:36:25 +0200

    entry tests done

--- src/tests/entry_test.py
+++ src/tests/entry_test.py
@@ -17,3 +17,31 @@ class TestEntry(unittest.TestCase):
             entry = Entry(test_data, Book(), MockKeygen())
 
         self.assertEqual(str(context.exception), "publisher not found")
+
+
+    def test_nonempty_optional_fields_are_being_added_to_entry_data(self):
+        test_data = {"author": "A", "title": "B", "publisher": "C", "year": 2000, "series": "D"}
+        entry = Entry(test_data, Book(), MockKeygen())
+
+        self.assertIn("series", entry.data.keys())
+
+
+    def test_empty_optional_fields_are_not_added_to_entry_data(self):
+        test_data = {"author": "A", "title": "B", "publisher": "C", "year": 2000, "series": ""}
+        entry = Entry(test_data, Book(), MockKeygen())
+
+        self.assertNotIn("series", entry.data.keys())
+
+
+    def test_field_formating_adds_quotation_marks_to_a_string(self):
+        test_data = {"author": "A", "title": "B", "publisher": "C", "year": 2000, "series": ""}
+        entry = Entry(test_data, Book(), MockKeygen())
+
+        self.assertEqual(entry._Entry__format_field("address", "northpole"), '"northpole"')
+
+
+    def test_field_formating_return_empty_if_key_not_found(self):
+        test_data = {"author": "A", "title": "B", "publisher": "C", "year": 2000}
+        entry = Entry(test_data, Book(), MockKeygen())
+
+        self.assertEqual(entry._Entry__format_field("dog's name", "fluffy"), "")
