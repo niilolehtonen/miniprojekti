f5e76b04449d24a6eba25ab285c3aa8ce9c2cca2
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-12-11 17:14:59 +0200

    deleted code that is not used anymore

--- src/entities/manual.py
+++ src/entities/manual.py
@@ -1,38 +1,4 @@
 class Manual:
-    #    def __init__(self, input_title: str,
-    #                 input_year: int,
-    #                 input_author: str,
-    #                 input_organization: str,
-    #                 input_address: str,
-    #                 input_edition: str,
-    #                 input_month: str,
-    #                 input_note: str,
-    #                 keygen
-    #                 ):
-    #        self.keygen = keygen
-    #
-    #        self.title = input_title
-    #        self.year = input_year
-    #        self.author = input_author
-    #        self.organization = input_organization
-    #        self.address = input_address
-    #        self.edition = input_edition
-    #        self.month = input_month
-    #        self.note = input_note
-    #
-    #    def format(self):
-    #        formated = "@manual{" + self.keygen.generate_key(self.title) + ","
-    #        formated += "\n  title        = \"" + self.title + "\","
-    #        formated += "\n  author       = \"" + self.author + "\","
-    #        formated += "\n  year         = " + str(self.year) + ","
-    #        formated += "\n  month        = \"" + str(self.month) + "\","
-    #        formated += "\n  address      = \"" + self.address + "\","
-    #        formated += "\n  note         = \"" + self.note + "\","
-    #        formated += "\n  organization = \"" + self.organization + "\","
-    #        formated += "\n  edition      = \"" + str(self.edition) + "\""
-    #        formated += "\n}"
-    #
-    #        return formated
 
     def get_required_fields(self):
         return [("title", str)]
--- src/index.py
+++ src/index.py
@@ -1,29 +1,5 @@
 from app import app
 
-# def main():
-#    while True:
-#        input_string = input('What would you like to do? add/fetch/quit: ')
-#        if input_string == "add":
-#            input_author = input("Give the name of the author: ")
-#            input_title = input("Give the book title: ")
-#            input_publisher = input("Give the name of the publisher: ")
-#            input_address = input("Give the address of the publisher: ")
-#            input_year = int(input("Give the year of the publishing: "))
-#            input_key = input("Give key name to book in latex: ")
-#
-#            book = Book(input_author, input_title, input_publisher, input_address, input_year, input_key)
-#
-#            file_saver = FileSaver("data.bib")
-#            file_saver.save(book)
-#
-#        elif input_string == "fetch":
-#            file_fetcher = FileFetcher("data.bib")
-#            print(file_fetcher.fetch())
-#
-#        elif input_string == "quit":
-#            break
-
-
 if __name__ == '__main__':
     # main()
     app.run(port=5001, debug=True)
